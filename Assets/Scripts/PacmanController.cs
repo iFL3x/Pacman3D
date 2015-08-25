1fb7519098fa1d30fe5879a52e7e582ff37b4cba
DeadlinezZ <fabrice-95@hotmail.com>
2015-08-25 19:31:53 +0200

    Rewrote Directions to use enums, fixed speedbug

--- Assets/Scripts/PacmanController.cs
+++ Assets/Scripts/PacmanController.cs
@@ -6,46 +6,47 @@ public class PacmanController : MonoBehaviour {
 
 	public float defSpeed = 0.02f;
 	private float speed;
-	private Vector3 curDirection;
-	public Vector3 plannedDirection;
-	public List<string> validDirections = new List<string>();
+	public Direction curDirection = Direction.Left;
+	public Direction plannedDirection = Direction.None;
+	public List<Direction> validDirections = new List<Direction>();
 	public LayerMask TileMask;
 	private GameObject curTile;
 	private bool stopping = false;
 	
 	void Start () {
-		curDirection = Vector3.right;
 		speed = defSpeed;
 	}
 
-	void FixedUpdate () {
+	void Update () {
 
 		if (Input.GetKey ("w")) {
-			plannedDirection = Vector3.forward;
+			plannedDirection = Direction.Forward;
 			StartPacman();
 		}
 
 		if (Input.GetKey ("a")) {
-			plannedDirection = Vector3.left;
+			plannedDirection =	Direction.Left;
 			StartPacman();
 		}
 
-		if (Input.GetKey ("s")) {
-			//curDirection = Vector3.back;
+		if (Input.GetKeyDown("s")) {
+			if(validDirections.Contains(GetBackDirection(curDirection))) {
+				transform.Rotate(0,180,0);
+			}
+			plannedDirection = Direction.None;
 			StartPacman();
 		}
 
 		if (Input.GetKey ("d")) {
-			plannedDirection = Vector3.right;
+			plannedDirection = Direction.Right;
 			StartPacman();
 		}
 
 		if (stopping && Vector3.Distance(new Vector3(curTile.transform.position.x, transform.position.y, curTile.transform.position.z), transform.position) < 0.1f) {
 			StopPacman();
 		}
-		transform.Translate (Vector3.forward * speed);
-
-
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	
 	}
 
 	private void StartPacman(){
@@ -81,41 +82,103 @@ public class PacmanController : MonoBehaviour {
 	private void CheckNeighbourTiles(){
 		Vector3 pacmanTilePos = curTile.transform.position;
 
-		//North
+		//Up
 		if (Physics.Raycast (new Vector3 (pacmanTilePos.x - 1, 1f, pacmanTilePos.z), Vector3.down, 1.5f, TileMask)) {
-			validDirections.Add("North");
+			validDirections.Add(Direction.Up);
 		}
 
-		//South
+		//Down
 		if (Physics.Raycast (new Vector3 (pacmanTilePos.x + 1, 1f, pacmanTilePos.z), Vector3.down, 1.5f, TileMask)) {
-			validDirections.Add("South");
+			validDirections.Add(Direction.Down);
 		}
 
-		//West
+		//Left
 		if (Physics.Raycast (new Vector3 (pacmanTilePos.x, 1f, pacmanTilePos.z - 1), Vector3.down, 1.5f, TileMask)) {
-			validDirections.Add("West");
+			validDirections.Add(Direction.Left);
 		}
 
-		//East
+		//Right
 		if (Physics.Raycast (new Vector3 (pacmanTilePos.x, 1f, pacmanTilePos.z + 1), Vector3.down, 1.5f, TileMask)) {
-			validDirections.Add("East");
+			validDirections.Add(Direction.Right);
 		}
 	}
 
 	private void CheckDirection(){
 		//If there is no direction command
-		if (plannedDirection == Vector3.zero) {
+		if (plannedDirection == Direction.None) {
 			//Check if pacman can go forward, else stop in the middle of the current tile
-			if (curDirection == Vector3.up && !validDirections.Contains ("North") ||
-				curDirection == Vector3.right && !validDirections.Contains ("East") ||
-				curDirection == Vector3.down && !validDirections.Contains ("South") ||
-				curDirection == Vector3.left && !validDirections.Contains ("West")) {
+			if (curDirection == Direction.Up && !validDirections.Contains (Direction.Up) ||
+			    curDirection == Direction.Right && !validDirections.Contains (Direction.Right) ||
+			    curDirection == Direction.Down && !validDirections.Contains (Direction.Down) ||
+			    curDirection == Direction.Left && !validDirections.Contains (Direction.Left)) {
 				stopping = true;
 			}  
 		} else if(validDirections.Count >= 3){
 			//Turn pacman based on plannedDirection and current Direction
-
+			if(plannedDirection == Direction.Left && validDirections.Contains(GetLeftDirection(curDirection))){
+				transform.Rotate(0,270,0);
+			} else if(plannedDirection == Direction.Right && validDirections.Contains(GetRightDirection(curDirection))){
+				transform.Rotate(0,90,0);
+			} 
 		}
 	}
+
+	private Direction GetLeftDirection(Direction currentDirection){
+		Direction dir = Direction.None;
+		if(currentDirection == Direction.Up){
+			dir = Direction.Left;
+		} else if(currentDirection == Direction.Right){
+			dir = Direction.Up;
+		} else if(currentDirection == Direction.Down){
+			dir = Direction.Right;
+		} else if(currentDirection == Direction.Left){
+			dir = Direction.Down;
+		} else {
+			dir = Direction.None;
+		}
+		return dir;
+	}
+	private Direction GetRightDirection(Direction currentDirection){
+		Direction dir = Direction.None;
+		if(currentDirection == Direction.Up){
+			dir = Direction.Right;
+		} else if(currentDirection == Direction.Right){
+			dir = Direction.Down;
+		} else if(currentDirection == Direction.Down){
+			dir = Direction.Left;
+		} else if(currentDirection == Direction.Left){
+			dir = Direction.Up;
+		} else {
+			dir = Direction.None;
+		}
+		return dir;
+	}
+
+	private Direction GetBackDirection(Direction currentDirection){
+		Direction dir = Direction.None;
+		if(currentDirection == Direction.Up){
+			dir = Direction.Down;
+		} else if(currentDirection == Direction.Right){
+			dir = Direction.Left;
+		} else if(currentDirection == Direction.Down){
+			dir = Direction.Up;
+		} else if(currentDirection == Direction.Left){
+			dir = Direction.Right;
+		} else {
+			dir = Direction.None;
+		}
+		return dir;
+	}
+
 	
+}
+
+public enum Direction {
+	None,
+	Up,
+	Right,
+	Down,
+	Left,
+	Forward,
+	Back
 }
